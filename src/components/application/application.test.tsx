2754791ba6454739dd14eb781cccaff380be7a3b
Chris <chris.gao77@gmail.com>
2022-10-01 23:00:17 -0400

    Tutorial 21

---
+++ src/components/application/application.test.tsx
@@ -0,0 +1,36 @@
+import Application from "./application"
+import { render, screen } from "@testing-library/react"
+
+describe("Application", () => {
+    test("renders correctly", () => {
+        render(<Application />)
+
+        const headingElelment = screen.getByRole("heading", {
+            level: 1
+        })
+        expect(headingElelment).toBeInTheDocument()
+
+        const bioElement = screen.getByRole("textbox", {
+            name: "Bio"
+        })
+        expect(bioElement).toBeInTheDocument()
+
+        const nameElement = screen.getByRole("textbox", {
+            name: "Name"
+        })
+        expect(nameElement).toBeInTheDocument()
+
+        const nameElement2 = screen.getByLabelText("Name")
+        expect(nameElement2).toBeInTheDocument()
+
+        const dropDownElement = screen.getByRole("combobox")
+        expect(dropDownElement).toBeInTheDocument()
+
+        const termsElement = screen.getByRole("checkbox")
+        expect(termsElement).toBeInTheDocument()
+
+        const buttonElement = screen.getByRole("button")
+        expect(buttonElement).toBeInTheDocument()
+    })
+
+})
