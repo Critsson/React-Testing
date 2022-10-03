cfc0f20e6e260556a823af1c75a2d423b3c5c209
Chris <chris.gao77@gmail.com>
2022-10-02 22:41:41 -0400

    Up to tutorial 42

--- src/hooks/use-counter/userCounter.test.tsx
+++ src/hooks/use-counter/userCounter.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, screen } from "@testing-library/react"
+import { renderHook, screen, act } from "@testing-library/react"
 import { useCounter } from "./useCounter"
 
 describe("useCounter", () => {
@@ -7,4 +7,24 @@ describe("useCounter", () => {
         expect(result.current.count).toBe(0)
     })
 
+    test("should accept and render the same initial count", () => {
+        const {result} = renderHook(useCounter, {
+            initialProps: {
+                initialCount: 10
+            }
+        })
+        expect(result.current.count).toBe(10)
+    })
+
+    test("should increment the count", () => {
+        const {result} = renderHook(useCounter)
+        act(() => result.current.increment())
+        expect(result.current.count).toBe(1)
+    })
+
+    test("should decrement the count", () => {
+        const {result} = renderHook(useCounter)
+        act(() => result.current.decrement())
+        expect(result.current.count).toBe(-1)
+    })
 })
